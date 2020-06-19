fd48368af6e6e71678906de47aef049d9be77a8f
Oliver <20156451+olie304@users.noreply.github.com>
2020-06-19 17:11:50 -0500

    Update InteractableBinaryStateChatTrigger.cs

--- InteractableBinaryStateChatTrigger.cs
+++ InteractableBinaryStateChatTrigger.cs
@@ -6,6 +6,8 @@ namespace SDG.Unturned
     // In order for it to work you must create a LevelObject that is on the same position as the parent of this script
     // and it must be a binary state object and it must have the same ID as specified in the ID slot below. I have not
     // been able to test everything so if you are interested and are running into problems leave an issue or contact me some other way.
+    // Final Note: In the code it is specified that the chat type must be local and there is a test I am unsure of where if
+    // LightingManager.day < LevelLighting.bias then the chat / toggle is ignored. Maybe it means it only works at night?
     public class InteractableBinaryStateChatTrigger : MonoBehaviour
     {
         [Tooltip("Object ID (Choose a random one)")]
