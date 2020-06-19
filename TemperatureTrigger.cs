0d8f6f0871437d86e77d16420b8ef7c561326529
Oliver <20156451+olie304@users.noreply.github.com>
2020-06-19 18:47:39 -0500

    Update TemperatureTrigger.cs

--- TemperatureTrigger.cs
+++ TemperatureTrigger.cs
@@ -6,18 +6,18 @@ namespace SDG.Unturned
     // The Bubble is positioned at the position of the Game Object the script is attatched to and has a radius of the X scale of the same Game Object's Transform component
     public class TemperatureTrigger : MonoBehaviour
     {
-    // Specifed effect to apply to player inside bubble
-    public EPlayerTemperature temperature;
+        // Specifed effect to apply to player inside bubble
+        public EPlayerTemperature temperature;
     }
 
     public enum EPlayerTemperature
     {
-    FREEZING,
-    COLD,
-    WARM,
-    BURNING,
-    NONE,
-    COVERED,
-    ACID
+        FREEZING,
+        COLD,
+        WARM,
+        BURNING,
+        NONE,
+        COVERED,
+        ACID
     }
 }
