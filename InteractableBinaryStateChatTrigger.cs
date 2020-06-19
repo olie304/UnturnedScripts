95aebbb4adf66abc070032c28f1e652a8bda562b
Oliver <20156451+olie304@users.noreply.github.com>
2020-06-19 17:05:19 -0500

    Update InteractableBinaryStateChatTrigger.cs

--- InteractableBinaryStateChatTrigger.cs
+++ InteractableBinaryStateChatTrigger.cs
@@ -2,8 +2,10 @@
 
 namespace SDG.Unturned
 {
-    // Toggles a binary state object when a phrase is typed in chat within a certain radius of the object
-    //DISCLAIMER: THIS MIGHT NOT WORK!
+    // Toggles a binary state object when a phrase is typed in chat within a certain radius of the object.
+    // In order for it to work you must create a LevelObject that is on the same position as the parent of this script
+    // and it must be a binary state object and it must have the same ID as specified in the ID slot below. I have not
+    // been able to test everything so if you are interested and are running into problems leave an issue or contact me some other way.
     public class InteractableBinaryStateChatTrigger : MonoBehaviour
     {
         [Tooltip("Object ID (Choose a random one)")]
@@ -14,9 +16,8 @@ namespace SDG.Unturned
 
         [Tooltip("Range of trigger")]
         public float sqrRadius;
-
-        [Tooltip("The Binary State Object to trigger")]
-        [SerializeField]
+        
+        // Ignore this, this is just to clarify the object type of the target as specified in the instructions above
         private InteractableObjectBinaryState target;
     }
 }
