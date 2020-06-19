4a26218d0c349dbac4641eda4b7139157211d000
Oliver <20156451+olie304@users.noreply.github.com>
2020-06-19 18:45:17 -0500

    Create TemperatureTrigger.cs

---
+++ TemperatureTrigger.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace SDG.Unturned
+{
+	// Creates a Temperature Bubble which applies the specified effect when the player is inside.
+    // The Bubble is positioned at the position of the Game Object the script is attatched to and has a radius of the X scale of the same Game Object's Transform component
+	public class TemperatureTrigger : MonoBehaviour
+	{
+		// Specifed effect to apply to player inside bubble
+		public EPlayerTemperature temperature;
+	}
+    
+    public enum EPlayerTemperature
+	{
+		FREEZING,
+		COLD,
+		WARM,
+		BURNING,
+		NONE,
+		COVERED,
+		ACID
+	}
+}
