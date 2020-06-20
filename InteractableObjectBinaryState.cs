28fbbfd3753a9f2f92397e2f24a76c11d5399604
Oliver <20156451+olie304@users.noreply.github.com>
2020-06-19 21:23:51 -0500

    InteractableObjectBinaryState.cs Unneeded
    It is added by the game at runtime anyways as long as your object is an intractable type

--- InteractableObjectBinaryState.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace SDG.Unturned
-{
-    // Apply this to your binary state Game Objects in order to use them with the chat event listener
-    public class InteractableObjectBinaryState : MonoBehaviour
-    {
-    }
-}
