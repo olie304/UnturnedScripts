5cae787fe603f82dc92f7611cd56e67f8f0aae47
Oliver <20156451+olie304@users.noreply.github.com>
2020-06-19 20:07:16 -0500

    Create CarepackageDestroy.cs

---
+++ CarepackageDestroy.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace SDG.Unturned
+{
+    // Applies enough Barricade damage to the base transform to destroy any Barricade 10 minutes after being instantiated.
+    // Object type should be a Barricade for this to do anything and the script should be placed on the Root Game Object
+    public class CarepackageDestroy : MonoBehaviour
+    {
+    }
+}
