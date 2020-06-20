531bfcd3ef5a86d503366af88964934e0520abff
Oliver <20156451+olie304@users.noreply.github.com>
2020-06-19 20:51:43 -0500

    Create Distraction.cs

---
+++ Distraction.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace SDG.Unturned
+{
+    // Upon being instatiated waits 2.5 seconds and then creates a zombie distraction with a radius of 24 meters.
+    // Destroys itself after first use, cannot be reset.
+    public class Distraction : MonoBehaviour
+    {
+    }
+}
