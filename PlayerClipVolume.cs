90be87b5987a57957c37d83318a901780107134f
Oliver <20156451+olie304@users.noreply.github.com>
2020-06-23 21:54:41 -0500

    Fixed indents

--- PlayerClipVolume.cs
+++ PlayerClipVolume.cs
@@ -8,12 +8,12 @@ using UnityEngine;
 namespace SDG.Framework.Devkit
 {
     // Requires a BoxCollider on the base Game Object
-	// Presumably prevents players from passing through the BoxCollider but not other entities?
+    // Presumably prevents players from passing through the BoxCollider but not other entities?
     // Game Object Layer is set to 21 at runtime and cannot be overriden
-	public class PlayerClipVolume : MonoBehaviour
-	{
+    public class PlayerClipVolume : MonoBehaviour
+    {
         // Whether or not to block the player from crossing through
-		[SerializeField]
-		protected bool _blockPlayer;
-	}
+        [SerializeField]
+        protected bool _blockPlayer;
+    }
 }
