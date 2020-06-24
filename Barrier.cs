82030b5b4daa57c12996cec6043b16743517a1bf
Oliver <20156451+olie304@users.noreply.github.com>
2020-06-23 21:53:04 -0500

    Fixed indents

--- Barrier.cs
+++ Barrier.cs
@@ -1,12 +1,10 @@
-using System;
-using Steamworks;
 using UnityEngine;
 
 namespace SDG.Unturned
 {
     // Requires a Collider with 'isTrigger' set to True
     // Instantly kills anything that enters the collider, not as many options as KillVolume.cs
-	public class Barrier : MonoBehaviour
-	{
-	}
+    public class Barrier : MonoBehaviour
+    {
+    }
 }
