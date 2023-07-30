4d2f7a96182fc57c2659a0d84d6837b61b3ca054
Robnewww <108140294+Robnewww@users.noreply.github.com>
2023-07-30 19:48:19 +0100

    Update StickyGrenade.cs
    MonoBehavior -> MonoBehaviour

--- StickyGrenade.cs
+++ StickyGrenade.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace SDG.Unturned
 {
     // When an object collides with the BoxCollider on the base Game Object, the base Game Object will become kinematic (stuck in place)
-    public class StickyGrenade : MonoBehavior
+    public class StickyGrenade : MonoBehaviour
     {
     }
 }
