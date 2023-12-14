fa22a0f92b5fb685bb72c250e41dc44f754f3c18
Rob <108140294+Robnewww@users.noreply.github.com>
2023-12-14 11:32:46 +0000

    Update Flashbang.cs
    Color and Audio

--- Flashbang.cs
+++ Flashbang.cs
@@ -1,10 +1,14 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace SDG.Unturned
 {
-    // Does what a flashbang does when the object is created at the end of the fuse length
-    public class Flashbang : MonoBehaviour
-    {
-        public float fuseLength = 2.5f;
+	// Does what a flashbang does when the object is created at the end of the fuse length
+	public class Flashbang : MonoBehaviour
+	{
+		public Color color = Color.white;
+		
+		public float fuseLength = 2.5f;
+		
+		public bool playAudioSource = true;
     }
 }
